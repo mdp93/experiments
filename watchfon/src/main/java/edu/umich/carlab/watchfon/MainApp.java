3d9f2cfa1d4e5ebfc34f3611f003a14f7be80ecf
Arun Ganesan <arunganesan123@gmail.com>
2019-01-16 18:41:35 -0500

    Using sensor stream for Main App

--- watchfon/src/main/java/edu/umich/carlab/watchfon/MainApp.java
+++ watchfon/src/main/java/edu/umich/carlab/watchfon/MainApp.java
@@ -4,7 +4,8 @@ import android.content.Context;
 import android.util.Pair;
 import edu.umich.carlab.CLDataProvider;
 import edu.umich.carlab.loadable.App;
-import edu.umich.carlab.loadable.SensorListAppBase;
+import edu.umich.carlabui.appbases.SensorListAppBase;
+import edu.umich.carlabui.appbases.SensorStreamAppBase;
 import edu.umich.carlab.sensors.PhoneSensors;
 import edu.umich.carlab.watchfon_estimates.MiddlewareImpl;
 
@@ -13,24 +14,28 @@ import edu.umich.carlab.watchfon_estimates.MiddlewareImpl;
  * Created by arunganesan on 6/24/18.
  */
 
-public class MainApp extends SensorListAppBase {
+public class MainApp extends SensorStreamAppBase {
     public MainApp(CLDataProvider cl, Context context) {
         super(cl, context);
         name = "PhoneCollectApp";
         description = "Only phone sensors";
 
+        final String watchfon_estimates = edu.umich.carlab.watchfon_estimates.MiddlewareImpl.APP;
+        final String world_aligned_imu = edu.umich.carlab.world_aligned_imu.MiddlewareImpl.APP;
+
         sensors.add(new Pair<>(PhoneSensors.DEVICE, PhoneSensors.GPS));
         sensors.add(new Pair<>(PhoneSensors.DEVICE, PhoneSensors.ACCEL));
         sensors.add(new Pair<>(PhoneSensors.DEVICE, PhoneSensors.GYRO));
 
-        sensors.add(new Pair<>(edu.umich.carlab.world_aligned_imu.MiddlewareImpl.APP, edu.umich.carlab.world_aligned_imu.MiddlewareImpl.ACCEL));
-        sensors.add(new Pair<>(edu.umich.carlab.world_aligned_imu.MiddlewareImpl.APP, edu.umich.carlab.world_aligned_imu.MiddlewareImpl.GYRO));
-        sensors.add(new Pair<>(edu.umich.carlab.watchfon_estimates.MiddlewareImpl.APP, MiddlewareImpl.SPEED));
-        sensors.add(new Pair<>(edu.umich.carlab.watchfon_estimates.MiddlewareImpl.APP, MiddlewareImpl.ODOMETER));
-        sensors.add(new Pair<>(edu.umich.carlab.watchfon_estimates.MiddlewareImpl.APP, MiddlewareImpl.FUEL));
-        sensors.add(new Pair<>(edu.umich.carlab.watchfon_estimates.MiddlewareImpl.APP, MiddlewareImpl.GEAR));
-        sensors.add(new Pair<>(edu.umich.carlab.watchfon_estimates.MiddlewareImpl.APP, MiddlewareImpl.ENGINERPM));
-        sensors.add(new Pair<>(edu.umich.carlab.watchfon_estimates.MiddlewareImpl.APP, MiddlewareImpl.STEERING));
+        subscribe(world_aligned_imu, edu.umich.carlab.world_aligned_imu.MiddlewareImpl.ACCEL);
+        subscribe(world_aligned_imu, edu.umich.carlab.world_aligned_imu.MiddlewareImpl.GYRO);
+        subscribe(watchfon_estimates, MiddlewareImpl.SPEED);
+        subscribe(watchfon_estimates, MiddlewareImpl.ODOMETER);
+        subscribe(watchfon_estimates, MiddlewareImpl.FUEL);
+        subscribe(watchfon_estimates, MiddlewareImpl.GEAR);
+        subscribe(watchfon_estimates, MiddlewareImpl.ENGINERPM);
+        subscribe(watchfon_estimates, MiddlewareImpl.STEERING);
 
+        addLineGraph(world_aligned_imu, edu.umich.carlab.world_aligned_imu.MiddlewareImpl.ACCEL_Y);
     }
 }
