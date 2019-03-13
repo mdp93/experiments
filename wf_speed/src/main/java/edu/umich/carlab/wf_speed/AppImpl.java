ae03a8751c436f2b5dcb72948d7c25a7b49d411e
Arun Ganesan <arunganesan123@gmail.com>
2019-03-13 12:25:33 -0400

    [wf_speed] Wired in watchfon_speed middleware

--- wf_speed/src/main/java/edu/umich/carlab/wf_speed/AppImpl.java
+++ wf_speed/src/main/java/edu/umich/carlab/wf_speed/AppImpl.java
@@ -1,28 +1,23 @@
 package edu.umich.carlab.wf_speed;
 
 import android.content.Context;
-import android.hardware.SensorManager;
-import android.util.Pair;
 import edu.umich.carlab.CLDataProvider;
 import edu.umich.carlab.DataMarshal;
-import edu.umich.carlab.loadable.App;
-import edu.umich.carlab.sensors.PhoneSensors;
+
 import edu.umich.carlabui.appbases.SensorListAppBase;
 
 
 public class AppImpl extends SensorListAppBase {
     final String TAG = "AppImpl";
 
+    edu.umich.carlab.watchfon_speed.MiddlewareImpl watchfon_speed =
+            new edu.umich.carlab.watchfon_speed.MiddlewareImpl();
 
     public AppImpl(CLDataProvider cl, Context context) {
         super(cl, context);
         foregroundApp = true;
         name = "wf_speed_app_impl";
-        subscribe(PhoneSensors.DEVICE, PhoneSensors.GRAVITY);
-        subscribe(PhoneSensors.DEVICE, PhoneSensors.MAGNET);
-        subscribe(PhoneSensors.DEVICE, PhoneSensors.GYRO);
-        subscribe(PhoneSensors.DEVICE, PhoneSensors.ACCEL);
-        subscribe(PhoneSensors.DEVICE, PhoneSensors.GPS);
+        subscribe(watchfon_speed.APP, watchfon_speed.SPEED);
     }
 
 
